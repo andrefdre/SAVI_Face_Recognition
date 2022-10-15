a1c11a946c80392d23e54cdb822921353ee3e8d3
André Cardoso <58526188+andrefdre@users.noreply.github.com>
2022-10-15 15:05:43 +0100

    test

--- Face_Detection.py
+++ Face_Detection.py
@@ -2,7 +2,7 @@ import cv2
 
 # Load the cascade
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
+#teastasdasfas
 # To capture video from webcam. 
 cap = cv2.VideoCapture(0)
 # To use a video file as input 
